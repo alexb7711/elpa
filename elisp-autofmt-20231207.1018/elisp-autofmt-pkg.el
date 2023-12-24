0f9f7801e1af9300e00f8b6209eba0bb8daa54ce
Alexander Brown <alex.brown7711@gmail.com>
2023-12-23 21:49:15 -0700

    Update

---
+++ elisp-autofmt-20231207.1018/elisp-autofmt-pkg.el
@@ -0,0 +1,12 @@
+(define-package "elisp-autofmt" "20231207.1018" "Emacs lisp auto-format"
+  '((emacs "29.1"))
+  :commit "6b280c854f964c303de2f9316f8f955e52893b8d" :authors
+  '(("Campbell Barton" . "ideasman42@gmail.com"))
+  :maintainers
+  '(("Campbell Barton" . "ideasman42@gmail.com"))
+  :maintainer
+  '("Campbell Barton" . "ideasman42@gmail.com")
+  :url "https://codeberg.org/ideasman42/emacs-elisp-autofmt")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
